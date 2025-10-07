d14e9d1507bcbbc795baa6fc9943bf985ffbeef4
SAkSak2006 <sakratallaharov@gmail.com>
2025-10-07 21:48:15 +0500

    v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt") // Для Room/Hilt аннотаций
+    id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
     id("androidx.room")
-    kotlin("kapt")
 }
 
 android {
@@ -30,13 +29,16 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    // ИСПРАВЛЕНО: Обновлено на Java 17
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
@@ -49,7 +51,6 @@ android {
         }
     }
 
-    // Фикс для Room: Указываем директорию схем (создастся автоматически)
     room {
         schemaDirectory("$projectDir/schemas")
     }
@@ -63,6 +64,7 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
     implementation("androidx.compose.ui:ui")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
@@ -71,7 +73,7 @@ dependencies {
     // Room
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
-    kapt("androidx.room:room-compiler:2.6.0")  // kapt для аннотаций
+    kapt("androidx.room:room-compiler:2.6.0")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
